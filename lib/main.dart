4841ea9effd0241a735b17d32177906ca1907ba1
TaeBbong <mok05289@korea.ac.kr>
2023-09-18 23:01:20 +0900

    feat: Init Project Structure with GetPages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_master/views/auth/signin/signin_page.dart';
+import 'package:flutter_getx_master/views/home/index_page.dart';
+import 'package:flutter_getx_master/views/home/todo/todo_page.dart';
+import 'package:get/get.dart';
+
+import 'views/splash_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +15,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return GetMaterialApp(
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => SplashPage()),
+        GetPage(name: '/auth/signin', page: () => SigninPage()),
+        GetPage(name: '/auth/signup', page: () => SigninPage()),
+        GetPage(name: '/home', page: () => IndexPage()),
+        GetPage(name: '/home/todo', page: () => TodoPage()),
+      ],
     );
   }
 }
