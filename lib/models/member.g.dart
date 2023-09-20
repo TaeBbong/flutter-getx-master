f09addcb7085566cb8fb22b82bb5194e1fd4363f
TaeBbong <mok05289@korea.ac.kr>
2023-09-20 23:44:18 +0900

    feat: Generate member model by freezed

---
+++ lib/models/member.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'member.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Member _$$_MemberFromJson(Map<String, dynamic> json) => _$_Member(
+      id: json['id'] as int,
+      email: json['email'] as String,
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$$_MemberToJson(_$_Member instance) => <String, dynamic>{
+      'id': instance.id,
+      'email': instance.email,
+      'name': instance.name,
+    };
